48f99a2710aa75979af06cdc51ff3490e904fc6f
Martin Salinas <msalinas@quadiontech.com>
2017-06-15 11:40:10 -0300

    vuelta de tuerca

--- js/secretWord.js
+++ js/secretWord.js
@@ -4,17 +4,29 @@ if (typeof module !== 'undefined') {
     module.exports = SecretWord;
 }
 
-function SecretWord(word) {
-    var hideChar = arguments[1] || '*';
+function SecretWord(word, hideChar = '*') {
+    this.hideChar = hideChar;
     this.revealedIds = [];
     this.revealedWord = hideChar.repeat(word.length).split("");
     this.text = word;
-    return new Proxy(this, {
-        get: (function(target, propertyKey, receiver) {
-            this.revealedIds.forEach(propertyAssigner(word, this.revealedWord), this);
+    this._reveal = function(pos) {
+        if (pos instanceof Array) {
+            return pos.map(this.reveal, this);
+        }
+        if (pos >= this.text.length) {
+          // error!
+        }
+        let l = this.text[pos];
+        this.revealedIds.push(pos);
+        this.revealedWord[pos] = l;
+        return l;
+    };
 
-            if (propertyKey === 'toString') {
-                return constant(this.revealedWord.join(""));
+    return new Proxy(SecretWord.prototype, {
+        get: (target, propertyKey, receiver) => {
+
+            if (propertyKey === 'length') {
+              return word.length;
             }
 
             if (propertyKey in SecretWord.prototype) {
@@ -30,39 +42,32 @@ function SecretWord(word) {
             }
 
             return this.revealedWord[propertyKey];
-        }).bind(this),
-        set: function(target, propertyKey, receiver) {
+        },
+        set: () => {}
+        /*function(target, propertyKey, receiver) {
             debugger
-        }
+        }*/
     });
 }
 
 SecretWord.prototype = {
     reveal: function(pos) {
-        if (pos instanceof Array) {
-            return pos.map(this.reveal, this);
-        }
-        this.revealedIds.push(pos);
-        return this.text[pos];
+      return this._reveal(pos);
     },
     occurrences: function(letter, fn) {
         return occurrences(this.text, letter, fn);
     },
     is: function(word) {
         return this.text === word;
+    },
+    try: function(letter) {
+      let matches = this.occurrences(letter);
+      this.reveal(matches);
+      return this.toString();
+    },
+    toString: function() {
+      return this.revealedWord.join("");
     }
 };
 
-
-/*function constant(v) {
-    return function() { return v; };
-}*/
 const constant = v => () => v;
-/*function copyProperty(from, to) {
-    return function(property) {
-        to[property] = from[property];
-    }
-};*/
-const propertyAssigner = (from, to) => (property) => {
-    to[property] = from[property]; return to;
-};
